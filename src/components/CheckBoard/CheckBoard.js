cd9ab6dc7c6356455081813099174b0a3ab49066
Pablo Cruz Ramos <A01138740@itesm.mx>
2023-08-24 13:39:36 -0600

    finished checkContainer layout

--- src/components/CheckBoard/CheckBoard.js
+++ src/components/CheckBoard/CheckBoard.js
@@ -17,12 +17,43 @@ export const BoardContainer = styled.div`
 
 export const CBoard = styled.div`
   height: 60vh;
-  background: grey;
+  padding: 30px;
+  background: blue;
   width: 70%;
   border-radius: 1vh;
+  display: flex;
+  justify-content: space-between;
   @media screen and (max-width: 768px) {
     width: 70%;
     height: 30vh;
     padding: 10px;
+  }
+`;
+
+export const CheckSection = styled.div`
+  display: flex-column;
+  width: 70%;
+  background: #add8e6;
+  @media screen and (max-width: 768px) {
+    width: 100%;
+  }
+`;
+
+export const CheckTitle = styled.div`
+  background: red;
+  width: 100%;
+  height: 5vh;
+`;
+
+export const CheckRow = styled.div`
+  background: orange;
+  width: 100%;
+`;
+
+export const PrinterImage = styled.div`
+  width: 30%;
+  background: green;
+  @media screen and (max-width: 768px) {
+    display: none;
   }
 `;
--- src/components/CheckBoard/index.js
+++ src/components/CheckBoard/index.js
@@ -1,10 +1,27 @@
 import React from "react";
-import { BoardContainer, CBoard } from "./CheckBoard";
+import {
+  BoardContainer,
+  CBoard,
+  CheckSection,
+  PrinterImage,
+  CheckRow,
+  CheckTitle,
+} from "./CheckBoard";
 
 const CheckBoard = () => {
   return (
     <BoardContainer>
-      <CBoard></CBoard>
+      <CBoard>
+        <CheckSection>
+          <CheckTitle>Title</CheckTitle>
+          <CheckRow>info con check</CheckRow>
+          <CheckRow>info con check</CheckRow>
+          <CheckRow>info con check</CheckRow>
+          <CheckRow>info con check</CheckRow>
+          <CheckRow>info con check</CheckRow>
+        </CheckSection>
+        <PrinterImage>Adios</PrinterImage>
+      </CBoard>
     </BoardContainer>
   );
 };
